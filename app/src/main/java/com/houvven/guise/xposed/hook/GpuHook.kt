d1702cf22650a85d69f8553bd76e7f938b1cc496
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-15 03:04:19 +0300

    Add optional OpenGL identity profile

---
+++ app/src/main/java/com/houvven/guise/xposed/hook/GpuHook.kt
@@ -0,0 +1,26 @@
+package com.houvven.guise.xposed.hook
+
+import android.opengl.GLES10
+import android.opengl.GLES20
+import com.houvven.guise.xposed.LoadPackageHandler
+import com.houvven.ktx_xposed.hook.beforeHookedMethod
+
+class GpuHook : LoadPackageHandler {
+    override fun onHook() {
+        hookGlIdentity(GLES10::class.java)
+        hookGlIdentity(GLES20::class.java)
+    }
+
+    private fun hookGlIdentity(clazz: Class<*>) {
+        clazz.beforeHookedMethod("glGetString", Int::class.javaPrimitiveType!!) { param ->
+            when (param.args.firstOrNull() as? Int) {
+                GLES20.GL_VENDOR -> config.gpuVendor.takeIf(String::isNotBlank)?.let {
+                    param.result = it
+                }
+                GLES20.GL_RENDERER -> config.gpuRenderer.takeIf(String::isNotBlank)?.let {
+                    param.result = it
+                }
+            }
+        }
+    }
+}
