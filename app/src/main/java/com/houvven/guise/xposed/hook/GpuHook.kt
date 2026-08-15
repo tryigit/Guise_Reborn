3596f4b0ce2189a11e66e19df3a1084e6b6aa481
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-15 03:39:30 +0300

    Apply renderer profile to Vulkan device name

--- app/src/main/java/com/houvven/guise/xposed/hook/GpuHook.kt
+++ app/src/main/java/com/houvven/guise/xposed/hook/GpuHook.kt
@@ -4,11 +4,15 @@ import android.opengl.GLES10
 import android.opengl.GLES20
 import com.houvven.guise.xposed.LoadPackageHandler
 import com.houvven.ktx_xposed.hook.beforeHookedMethod
+import com.houvven.ktx_xposed.logger.XposedLogger
 
 class GpuHook : LoadPackageHandler {
     override fun onHook() {
         hookGlIdentity(GLES10::class.java)
         hookGlIdentity(GLES20::class.java)
+        config.gpuRenderer.takeIf(String::isNotBlank)?.let { renderer ->
+            VulkanPrivacyBridge.configure(renderer).onFailure { XposedLogger.e(it) }
+        }
     }
 
     private fun hookGlIdentity(clazz: Class<*>) {
